40faa79abb8d8cfd9c1a2996b3f5c637d67222e9
FrancisChung <francis.c.chung@gmail.com>
2022-03-04 06:27:17 +1100

    Map Example

--- variables/main.tf
+++ variables/main.tf
@@ -16,3 +16,11 @@ variable "mylist" {
   type = list(string)
   default = ["Value1", "Value2", "Value3"]
 }
+
+variable "mymap" {
+  type = map
+  default = {
+    key1 = "Value1"
+    key2 = "Value2"
+  }
+}
