43f54577cf3d73a53deaf7a1ef9a3a55cd147156
FrancisChung <francis.c.chung@gmail.com>
2022-03-07 06:55:52 +1100

    baseline Security Group main.tf

--- sg/main.tf
+++ sg/main.tf
@@ -1,27 +1,8 @@
 provider "aws" {
-    region = "eu-west-2"
+    region = "ap-southeast-2"
 }
 
 resource "aws_instance" "ec2" {
-    ami = "ami-032598fcc7e9d1c7a"
+    ami = "ami-089e8a52c00cede0b"
     instance_type = "t2.micro"
-    security_groups = [aws_security_group.webtraffic.name]
-}
-
-resource "aws_security_group" "webtraffic" {
-    name = "Allow HTTPS"
-
-    ingress {
-        from_port = 443
-        to_port = 443
-        protocol = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 443
-        to_port = 443
-        protocol = "TCP"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
 }
