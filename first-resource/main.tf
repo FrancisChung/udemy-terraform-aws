1b1be8da8ab00725e2262af9fc17d4e2ef0a5313
FrancisChung <francis.c.chung@gmail.com>
2022-03-07 06:51:39 +1100

    ElasticIP Tutorial

--- first-resource/main.tf
+++ first-resource/main.tf
@@ -6,3 +6,11 @@ resource "aws_instance" "ec2" {
     ami = "ami-089e8a52c00cede0b"
     instance_type = "t2.micro"
 }
+
+resource "aws_eip" "elasticeip" {
+    instance = aws_instance.ec2.id
+}
+
+output "EIP" {
+    value = aws_eip.elasticeip.public_ip
+}
