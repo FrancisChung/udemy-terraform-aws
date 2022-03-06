cb3252066358e47a694819cc6f608c6e2c695209
FrancisChung <francis.c.chung@gmail.com>
2022-03-07 07:08:59 +1100

    Security Groups Lesson

--- sg/main.tf
+++ sg/main.tf
@@ -5,4 +5,22 @@ provider "aws" {
 resource "aws_instance" "ec2" {
     ami = "ami-089e8a52c00cede0b"
     instance_type = "t2.micro"
+    security_groups = [aws_security_group.webtraffic.name]
+}
+
+resource "aws_security_group" "webtraffic" {
+    name = "Allow HTTPS"
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 443
+        to_port = 443
+        protocol = "TCP"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
