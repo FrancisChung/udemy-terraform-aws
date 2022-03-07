7e68732236190dd98984be84dae24e402b6658f9
FrancisChung <francis.c.chung@gmail.com>
2022-03-07 11:32:51 +1100

    Dynamic Group lesson baseline

--- dynamic/main.tf
+++ dynamic/main.tf
@@ -1,45 +1,26 @@
 provider "aws" {
-    region = "eu-west-2"
-}
-
-variable "ingressrules" {
-    type = list(number)
-    default = [80,443]
-}
-
-variable "egressrules" {
-    type = list(number)
-    default = [80,443,25,3306,53,8080]
+    region = "ap-southeast-2"
 }
 
 resource "aws_instance" "ec2" {
-    ami = "ami-032598fcc7e9d1c7a"
+    ami = "ami-089e8a52c00cede0b"
     instance_type = "t2.micro"
     security_groups = [aws_security_group.webtraffic.name]
 }
 
 resource "aws_security_group" "webtraffic" {
     name = "Allow HTTPS"
-
-    dynamic "ingress" {
-        iterator = port 
-        for_each = var.ingressrules
-        content {
-        from_port = port.value
-        to_port = port.value
+    ingress {
+        from_port = 443
+        to_port = 443
         protocol = "TCP"
         cidr_blocks = ["0.0.0.0/0"]
-        }
     }
 
-    dynamic "egress" {
-        iterator = port 
-        for_each = var.egressrules
-        content {
-        from_port = port.value
-        to_port = port.value
+    egress {
+        from_port = 443
+        to_port = 443
         protocol = "TCP"
         cidr_blocks = ["0.0.0.0/0"]
-        }
     }
 }
